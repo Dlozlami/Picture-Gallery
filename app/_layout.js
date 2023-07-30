05741150c5af73e84d50481da5fe787863ae842a
Dlozi <dlozi.mthethwa@gmail.com>
2023-07-30 17:34:22 +0200

    first

---
+++ app/_layout.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default App(){
+  return (
+    <View>
+      <Text>_layout</Text>
+    </View>
+  )
+}
+
+
+
+const styles = StyleSheet.create({})
