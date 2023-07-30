2276f12c54235404168de4c507bf5add550ac8cf
Dlozi <dlozi.mthethwa@gmail.com>
2023-07-30 17:34:44 +0200

    layout

---
+++ app/tools/camera.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Camera(){
+  return (
+    <View>
+      <Text>camera</Text>
+    </View>
+  )
+}
+
+
+
+const styles = StyleSheet.create({})
